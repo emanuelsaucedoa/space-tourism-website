01ea5b977743413dd8a387cf15619dcb350c0a2d
Emanuel Saucedo <emasaucedodev3@gmail.com>
2024-01-01 13:21:34 -0300

    Add advance in routing

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let container = document.getElementsByClassName('container');
+let home = document.getElementById('home');
+let destination = document.getElementById('destination');
+let moon;
+let mars;
+let europa;
+let titan;
+
+
+
+
